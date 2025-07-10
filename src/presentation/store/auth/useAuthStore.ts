3f19d2e1e62a8e90ce3ff1923d6b306478015f1f
Gerardo de León García <gerardo-leon10@hotmail.com>
2025-07-10 14:19:16 -0600

    Auth Store - Central source of information

---
+++ src/presentation/store/auth/useAuthStore.ts
@@ -0,0 +1,33 @@
+import {create} from 'zustand';
+import {User} from '../../../domain/entities/user';
+import {AuthStatus} from '../../../infrastructure/interfaces/auth.status';
+import {authLogin} from '../../../actions/auth/auth';
+
+export interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+  login: (email: string, password: string) => Promise<boolean>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const useAuthStore = create<AuthState>()((set, get) => ({
+  status: 'checking',
+  token: undefined,
+  user: undefined,
+
+  login: async (email, password) => {
+    const response = await authLogin(email, password);
+    if (!response) {
+      set({status: 'unauthenticated', token: undefined, user: undefined});
+      return false;
+    }
+    // TODO: Save token and user in storage
+    set({
+      status: 'authenticated',
+      token: response.token,
+      user: response.user,
+    });
+    return true;
+  },
+}));
