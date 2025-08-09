be7dffb7aabf38055f82aa383cf7aae331888867
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2025-08-09 12:20:58 -0600

    Log out

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,18 @@
 import {Button, Layout, Text} from '@ui-kitten/components';
 import React from 'react';
 import Icon from '@react-native-vector-icons/ionicons';
+import {useAuthStore} from '../../store/auth/useAuthStore';
 
 export const HomeScreen = () => {
+  const {logout} = useAuthStore();
+
   return (
     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text category="h1">H1</Text>
 
-      <Button accessoryLeft={<Icon name="logo-facebook" size={20} />}>
+      <Button
+        onPress={logout}
+        accessoryLeft={<Icon name="log-out-outline" size={20} />}>
         Cerrar sesión
       </Button>
     </Layout>
